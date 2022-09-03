62ce5beb6d0341689433e361df1fc2a1075d8698
shourovahmd <moh.abuhanifa@gmail.com>
2022-09-04 02:20:01 +0600

    fetched news

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const category = document.getElementById("category");
+const ul_class = "navbar-nav me-auto mb-2 mb-lg-0";
 window.addEventListener("load", (event) => {
   const url = "https://openapi.programming-hero.com/api/news/categories";
   fetch(url)
@@ -10,11 +11,21 @@ function displayCategory(data) {
   const categories = data.data.news_category;
   categories.forEach((cat) => {
     const ul = document.createElement("ul");
+    ul.className = ul_class;
     const li = `<li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">${cat.category_name}</a>
+                <a class="nav-link" aria-current="page" href="#" onclick=loadNews(${cat.category_id})> ${cat.category_name}</a>
                 </li>`;
     ul.innerHTML = li;
+    category.appendChild(ul);
     console.log(cat);
   });
-  console.log(categories);
+}
+
+function loadNews(id) {
+  const param = 0 + `${id}`;
+  const url = `https://openapi.programming-hero.com/api/news/category/${param}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+  console.log(url);
 }
